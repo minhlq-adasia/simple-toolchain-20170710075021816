d10cf6790b70f769081dba7c0bde1be351115b1d
Minh Lam <minhlq@adasiaholdings.com>
2017-07-10 16:43:17 +0700

    f

---
+++ htdocs/test.js
@@ -0,0 +1,11 @@
+// Casperインスタンス生成
+var casper = require('casper').create({
+  viewportSize: { width: 375, height: 667 }
+});
+
+var BASE_URL = 'https://www.facebook.com/';
+casper.start(BASE_URL);
+
+casper.then(function(){
+  casper.echo('dsadsad');
+})
